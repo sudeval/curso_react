888a586d7b26bb7942969794152a33aac0957d56
Tiago Sudeval Ferreira Duarte <sudeval@gmail.com>
2018-11-08 12:50:26 -0300

    transformando o dashboard em classe

--- my-money-app/frontend/src/dashboard/dashboard.jsx
+++ my-money-app/frontend/src/dashboard/dashboard.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
 
+import Content from '../common/template/content'
+import ContentHeader from '../common/template/contentHeader'
 
-export default props => (
-    <h1>Dashboard</h1>
-)
+class Dashboard extends Component {
+
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Dashboard' small='Versao 1.0' />
+                <Content>
+                    Dashboard
+            </Content>
+            </div>
+        )
+    }
+}
+
+export default Dashboard
