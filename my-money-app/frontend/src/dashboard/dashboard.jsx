f656e8ef73b41aded3c41c716d650de374cadfc1
Tiago Sudeval Ferreira Duarte <sudeval@gmail.com>
2018-11-12 08:52:49 -0300

    dashboard inicial

--- my-money-app/frontend/src/dashboard/dashboard.jsx
+++ my-money-app/frontend/src/dashboard/dashboard.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 
 import Content from '../common/template/content'
 import ContentHeader from '../common/template/contentHeader'
+import Row from '../common/layout/row'
+import ValueBox from '../common/widget/valueBox'
 
 class Dashboard extends Component {
 
@@ -10,8 +12,15 @@ class Dashboard extends Component {
             <div>
                 <ContentHeader title='Dashboard' small='Versao 1.0' />
                 <Content>
-                    Dashboard
-            </Content>
+                    <Row>
+                        <ValueBox cols='12, 4' color='green' icon='bank'
+                            value='R$ 10' text='Total de Créditos' />
+                        <ValueBox cols='12, 4' color='red' icon='credit-card'
+                            value='R$ 10' text='Total de Débitos' />
+                        <ValueBox cols='12, 4' color='blue' icon='money'
+                            value='R$ 0' text='Valor Consolidado' />
+                    </Row>
+                </Content>
             </div>
         )
     }
