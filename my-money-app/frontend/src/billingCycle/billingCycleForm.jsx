dd1daf48bb8aed61706483b293ef4af4a27bcbb8
Tiago Sudeval Ferreira Duarte <sudeval@gmail.com>
2018-11-17 00:49:11 -0300

    adicionando o creditList ao ciclo de pagamentos

--- my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,29 +1,33 @@
 import React, { Component } from 'react'
 import { reduxForm, Field } from 'redux-form'
-import labelAndInput from '../common/form/labelAndInput'
+import LabelAndInput from '../common/form/labelAndInput'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
 import { init } from './billingCycleActions'
+import CreditList from './creditList'
 
 class BillingCycleForm extends Component {
     render() {
-        const { handleSubmit } = this.props
+        const { handleSubmit, readOnly } = this.props
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
-                    <Field name='name' component={labelAndInput}
-                        label='Nome' cols='12 4' placeholder='Informe o nome'/>
-                    <Field name='month' component={labelAndInput} 
+                    <Field name='name' component={LabelAndInput} readOnly={readOnly}
+                        label='Nome' cols='12 4' placeholder='Informe o nome' />
+                    <Field name='month' component={LabelAndInput} readOnly={readOnly}
                         label='Mês' cols='12 4' placeholder='Informe o mês' type='number'/>
-                    <Field name='year' component={labelAndInput} 
+                    <Field name='year' component={LabelAndInput}  readOnly={readOnly}
                         label='Ano' cols='12 4' placeholder='Informe o ano' type='number'/>
+
+                    <CreditList cols='12 6' readOnly={readOnly}/>
                 </div>
                 <div className='box-footer'>
-                    <button type='submit' className='btn btn-primary'
-                    >Enviar</button>
-                    <button type='button' className='btn btn-default' onClick={this.props.init}
-                    >Cancelar</button>
+                    <button type='submit' className={`btn btn-${this.props.submitClass}`}>
+                        {this.props.submitLabel}
+                    </button>
+                    <button type='button' className='btn btn-default'
+                        onClick={this.props.init}>Cancelar</button>
                 </div>
             </form>
         )
